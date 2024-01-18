3b18949905a5e9a8e858224ed07aeb68691ad2ea
bingjetli <bingjetli88@gmail.com>
2024-01-18 13:51:54 -0500

    implemented loading from localstorage

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let timer_list = [];
 
 
 
-const createDivider = _ => {
+const DividerComponent = _ => {
   const divider = document.createElement('div');
   divider.classList.add('divider');
   divider.innerHTML = '&nbsp;';
@@ -21,6 +21,276 @@ const createTaskControlsContainer = children => {
 
 
   return container;
+};
+
+
+const TaskComponent = (
+  task_key,
+  parent_container,
+  new_task_button,
+) => {
+
+  //Create a reference to the `task id`, if a `task_key` was given,
+  //extract it from the `task_key` otherwise generate a new id using
+  //the current unix timestamp.
+  const task_id = task_key === null ?
+    Date.now() :
+    task_key.split('-')[1];
+
+  //Create the task container HTML element. And set the element id to
+  //the task key that will be used to reference the local storage. If
+  //a `task_key` was provided, use that instead. Otherwise, generate 
+  //a new one.
+  const task_div = document.createElement('div');
+  task_div.id = 'task-' + task_id;
+  task_div.classList.add('task');
+
+
+  //Now try to load the task data from the local storage if a `task_key`
+  //was provided.
+  let stored_task = null;
+  if (task_key !== null) {
+    const value_string = localStorage.getItem(task_key);
+    if (value_string === null) alert('Bug detected, failed to load ' + task_key + ' from the local storage.');
+    else {
+      stored_task = JSON.parse(value_string);
+    }
+  }
+
+
+  //Now, create the title element. And append it to the task container.
+  const task_title_button = document.createElement('button');
+  task_title_button.innerText = stored_task !== null ?
+    stored_task.name :
+    'task-' + task_id;
+  task_title_button.id = 'task-' + task_id + '-title';
+  task_title_button.classList.add('clickable', 'task-title');
+  task_title_button.onclick = _ => renameTitle({
+    triggerButton: task_title_button,
+    key: 'task-' + task_id,
+  });
+  task_div.append(task_title_button);
+
+
+  //Now create the task timer display element.
+  const task_duration_span = document.createElement('span');
+  task_duration_span.innerText = stored_task === null ?
+    toTimeString(0) :
+    toTimeString(stored_task.duration);
+  task_duration_span.id = 'task-' + task_id + '-timer';
+  task_div.append(task_duration_span);
+
+
+  if (stored_task === null) {
+
+    //Create an object to store timer details. And add this object on
+    //to the list of active timers. This also starts the timer countdown.
+    const timer_object = {
+      element: task_duration_span,
+      startTime: Date.now(),
+      isRemovedOnTick: false,
+      removeOnTick: function () { this.isRemovedOnTick = true; },
+    };
+    timer_list.push(timer_object);
+
+
+    //Now create the button to stop the timer.
+    //Saving a task will stop the timer and show the `new task` button
+    //again.
+    if (project_container === null || new_task_button === null) {
+      alert('bug detected, trying to create a new task component but the parameters `project_container` or `new_task_button` are null.');
+    }
+    const save_task_button = document.createElement('button');
+    save_task_button.id = 'task-' + task_id + '-save-button';
+    save_task_button.innerText = 'save task';
+    save_task_button.classList.add('clickable');
+    save_task_button.onclick = _ => saveTask({
+      parentContainer: parent_container,
+      saveTaskButton: save_task_button,
+      newTaskButton: new_task_button,
+      taskTimer: timer_object,
+      taskTitleElement: task_title_button,
+    });
+
+
+    const save_button_container = document.createElement('div');
+    save_button_container.classList.add('task-controls-container');
+    save_button_container.append(save_task_button);
+    task_div.append(save_button_container);
+  }
+
+
+  return task_div;
+};
+
+
+const TaskGroupComponent = task_group_key => {
+
+  const task_group_id = task_group_key === null ?
+    Date.now() :
+    task_group_key.split('-')[2];
+  const task_group_div = document.createElement('div');
+  task_group_div.id = 'task-group-' + task_group_id;
+  task_group_div.classList.add('task-group');
+
+
+  //Try to load the task group data from local storage.
+  let stored_task_group = null;
+  if (task_group_key !== null) {
+    const value_string = localStorage.getItem(task_group_key);
+    if (value_string === null) alert('bug detected, failed to load ' + task_group_key + ' from the localstorage.');
+    else {
+      stored_task_group = JSON.parse(value_string);
+    }
+  }
+
+
+  const title_section = document.createElement('div');
+  title_section.classList.add('task-group-title-container');
+
+  const task_group_title_button = document.createElement('button');
+  task_group_title_button.innerText = stored_task_group === null ?
+    'task-group-' + task_group_id :
+    stored_task_group.name;
+  task_group_title_button.id = 'task-group-' + task_group_id + '-title';
+  task_group_title_button.classList.add('clickable', 'task-group-title');
+  task_group_title_button.onclick = _ => renameTitle({
+    triggerButton: task_group_title_button,
+    key: 'task-group-' + task_group_id,
+  });
+  title_section.append(task_group_title_button);
+  task_group_div.append(title_section);
+
+
+  const tasks_section = document.createElement('div');
+  tasks_section.id = 'task-group-task-' + task_group_id + '-container';
+  tasks_section.classList.add('task-group-task-container');
+
+  //Draw existing tasks inside the tasks section if they exist.
+  if (stored_task_group !== null) {
+    stored_task_group.tasks.forEach(task_key => {
+      tasks_section.append(TaskComponent(task_key, null, null));
+    });
+  }
+
+  const new_task_button = document.createElement('button');
+  new_task_button.innerText = 'new task';
+  new_task_button.classList.add('clickable');
+  new_task_button.onclick = _ => createNewTask({
+    parentContainer: tasks_section,
+    newTaskButton: new_task_button,
+  });
+  tasks_section.append(createTaskControlsContainer(new_task_button));
+  task_group_div.append(tasks_section);
+
+
+  return task_group_div;
+};
+
+
+
+
+const ProjectComponent = project_key => {
+
+  //The `project_id` is a unix timestamp of the current time when the 
+  //new project was created. This ensures that the project IDs are 
+  //mostly unique.
+  const project_id = project_key === null ?
+    Date.now() :
+    project_key.split('-')[1];
+  const project_div = document.createElement('div');
+  project_div.id = project_key === null ?
+    'project-' + project_id :
+    project_key;
+  project_div.classList.add('project');
+
+
+  //Try to retreive the project from localstorage if a project key was
+  //provided.
+  let stored_project = null;
+  if (project_key !== null) {
+
+    //The user provided a project key to an existing project, so we'll
+    //try to load it from localstorage.
+    const value_string = localStorage.getItem(project_key);
+
+    if (value_string !== null) {
+
+      //If the project was successfully loaded from the localStorage,
+      //we convert it to JSON and save it into `stored_project`;
+      stored_project = JSON.parse(value_string);
+    }
+    else alert('Bug detected, tried to load "' + project_key + '" but it doesn\'t exist in the localstorage.');
+  }
+
+
+  //Draw the project title.
+  const project_title_button = document.createElement('button');
+  project_title_button.innerText = stored_project === null ?
+    'project-' + project_id :
+    stored_project.name;
+  project_title_button.id = 'project-' + project_id + '-title';
+  project_title_button.classList.add('clickable', 'project-title');
+  project_title_button.onclick = _ => renameTitle({
+    triggerButton: project_title_button,
+    key: 'project-' + project_id,
+  });
+  project_div.append(project_title_button);
+
+
+  if (stored_project !== null) {
+    if (
+      stored_project.data !== null &&
+      stored_project.data !== undefined
+    ) {
+
+      //Draw the tasks or task groups associated with this project.
+      stored_project.data.forEach(key => {
+        if (key.startsWith('task-group-')) project_div.append(
+          DividerComponent(),
+          TaskGroupComponent(key)
+        );
+        else if (key.startsWith('task-')) project_div.append(
+          DividerComponent(),
+          TaskComponent(key, null, null)
+        );
+      });
+    }
+    else alert('Bug detected, the project loaded from localstorage might be corrupted.');
+  }
+
+
+  //Create the task group creation button.
+  const new_task_group_button = document.createElement('button');
+  new_task_group_button.innerText = 'new task group';
+  new_task_group_button.classList.add('clickable');
+  new_task_group_button.onclick = _ => createNewTaskGroup({
+    projectKey: project_div.id,
+    newTaskGroupButton: new_task_group_button,
+  });
+
+
+  //Create the task creation button.
+  const new_task_button = document.createElement('button');
+  new_task_button.innerText = 'new task';
+  new_task_button.classList.add('clickable');
+  new_task_button.onclick = _ => createNewTask({
+    parentContainer: project_div,
+    newTaskButton: new_task_button,
+  });
+
+
+  //Draw the project controls.
+  const controls_div = document.createElement('div');
+  controls_div.classList.add('new-buttons-container');
+  controls_div.append(new_task_group_button, new_task_button);
+  project_div.append(
+    DividerComponent(),
+    controls_div,
+  );
+
+
+  return project_div;
 };
 
 
@@ -58,8 +328,17 @@ window.onload = _ => {
   project_container = document.getElementById('project-container');
   if (project_container !== null) {
 
-    //TODO: Use localstorage to check for stored projects. If there are
-    //existing projects, draw them in the DOM.
+    //If there are existing projects, draw them in the DOM.
+    let project_list = [];
+    const value_string = localStorage.getItem('project-list');
+    if (value_string !== null) {
+
+      //This means there might be existing projects in the localstorage.
+      project_list = JSON.parse(value_string);
+    }
+    project_list.forEach(project_key => {
+      project_container.append(ProjectComponent(project_key));
+    });
 
 
     //After drawing the existing projects, draw the `new_project` button.
@@ -67,6 +346,7 @@ window.onload = _ => {
     new_project_button.innerText = 'new project';
     new_project_button.onclick = _ => createNewProject({
       newProjectButton: new_project_button,
+      projectList: project_list,
     });
     new_project_button.classList.add('clickable');
     project_container.append(new_project_button);
@@ -98,85 +378,30 @@ window.onload = _ => {
 
 
 function createNewProject({
-  newProjectButton
+  newProjectButton,
+  projectList,
 }) {
 
-  //The `project_id` is a unix timestamp of the current time when the 
-  //new project was created. This ensures that the project IDs are 
-  //mostly unique.
-  const project_id = Date.now();
-  const new_project = document.createElement('div');
-  new_project.id = 'project-' + project_id + '-container';
-  new_project.classList.add('project');
-
-
-  const new_task_group_button = document.createElement('button');
-  new_task_group_button.innerText = 'new task group';
-  new_task_group_button.classList.add('clickable');
-  new_task_group_button.onclick = _ => createNewTaskGroup({
-    projectContainer: new_project,
-    newTaskGroupButton: new_task_group_button,
-  });
-
-
-  const new_task_button = document.createElement('button');
-  new_task_button.innerText = 'new task';
-  new_task_button.classList.add('clickable');
-  new_task_button.onclick = _ => createNewTask({
-    parentContainer: new_project,
-    newTaskButton: new_task_button,
-  });
-
-
-  const new_buttons_container = document.createElement('div');
-  new_buttons_container.classList.add('new-buttons-container');
-  new_buttons_container.append(new_task_group_button, new_task_button);
-
-
-  const rename_title_button = document.createElement('button');
-  rename_title_button.innerText = 'project-' + project_id;
-  rename_title_button.id = 'project-' + project_id + '-title';
-  rename_title_button.classList.add('clickable', 'project-title');
-  rename_title_button.onclick = _ => renameTitle({
-    triggerButton: rename_title_button,
-    key: 'project-' + project_id,
-  });
-
-  new_project.append(
-    rename_title_button,
-    createDivider(),
-    new_buttons_container,
-  );
+  const new_project = ProjectComponent(null);
   newProjectButton.replaceWith(new_project);
   new_project.after(newProjectButton);
 
 
-  //Update the localstorage.
-  let project_list = localStorage.getItem('project-list');
-  if (project_list === null) localStorage.setItem(
+  //Push the project.id which contains the project key onto the list
+  //of projects.
+  projectList.push(new_project.id);
+
+
+  //Then update the value in the localstorage.
+  localStorage.setItem(
     'project-list',
-    JSON.stringify(['project-' + project_id]),
+    JSON.stringify(projectList),
   );
-  else {
-    //Parse the `project_list` into it's actual datatype.
-    project_list = JSON.parse(project_list);
 
 
-    //Add the new project_id to the project list.
-    // @ts-ignore
-    project_list.push('project-' + project_id);
-
-
-    //Update the localstorage item.
-    localStorage.setItem(
-      'project-list',
-      JSON.stringify(project_list),
-    );
-
-  }
   //Create the project item in localstorage.
-  localStorage.setItem('project-' + project_id, JSON.stringify({
-    name: rename_title_button.innerText,
+  localStorage.setItem(new_project.id, JSON.stringify({
+    name: new_project.id,
     data: [],
   }));
 }
@@ -187,142 +412,48 @@ function createNewTask({
   newTaskButton
 }) {
 
-  const task_id = Date.now();
-  const new_task = document.createElement('div');
-  new_task.id = 'task-' + task_id + '-container';
-  new_task.classList.add('task');
-
-
-  const rename_title_button = document.createElement('button');
-  rename_title_button.innerText = 'task-' + task_id;
-  rename_title_button.id = 'task-' + task_id + '-title';
-  rename_title_button.classList.add('clickable', 'task-title');
-  rename_title_button.onclick = _ => renameTitle({
-    triggerButton: rename_title_button,
-    key: 'task-' + task_id,
-  });
-
-
-  const timer = document.createElement('span');
-  timer.innerText = toTimeString(0);
-  timer.id = 'task-' + task_id + '-timer';
-  const timer_object = {
-    element: timer,
-    startTime: Date.now(),
-    isRemovedOnTick: false,
-    removeOnTick: function () { this.isRemovedOnTick = true; },
-  };
-  timer_list.push(timer_object);
-
-
-  //Saving a task will stop the timer and show the `new task` button
-  //again.
-  const save_task_button = document.createElement('button');
-  save_task_button.id = 'task-' + task_id + '-save-button';
-  save_task_button.innerText = 'save task';
-  save_task_button.classList.add('clickable');
-  save_task_button.onclick = _ => saveTask({
-    parentContainer: parentContainer,
-    saveTaskButton: save_task_button,
-    newTaskButton: newTaskButton,
-    taskTimer: timer_object,
-    taskTitleElement: rename_title_button,
-  });
-
-
-  const save_button_container = document.createElement('div');
-  save_button_container.classList.add('task-controls-container');
-  save_button_container.append(save_task_button);
-
-
-  new_task.append(
-    rename_title_button,
-    timer,
-    save_button_container,
-  );
+  const new_task = TaskComponent(null, parentContainer, newTaskButton);
   if (
     newTaskButton.parentNode !== null &&
     newTaskButton.parentNode.classList.contains('new-buttons-container') === true
   ) {
     newTaskButton.parentNode.replaceWith(new_task);
-    new_task.after(createDivider());
+    new_task.after(DividerComponent());
   }
   else newTaskButton.replaceWith(new_task);
-
-
 }
 
 
 
 function createNewTaskGroup({
-  projectContainer,
+  projectKey,
   newTaskGroupButton,
 }) {
 
-  const task_group_id = Date.now();
-  const new_task_group = document.createElement('div');
-  new_task_group.id = 'task-group-' + task_group_id + '-container';
-  new_task_group.classList.add('task-group');
-
-
-  const title_container = document.createElement('div');
-  title_container.classList.add('task-group-title-container');
-  const rename_title_button = document.createElement('button');
-  rename_title_button.innerText = 'task-group-' + task_group_id;
-  rename_title_button.id = 'task-group-' + task_group_id + '-title';
-  rename_title_button.classList.add('clickable', 'task-group-title');
-  rename_title_button.onclick = _ => renameTitle({
-    triggerButton: rename_title_button,
-    key: 'task-group-' + task_group_id,
-  });
-  title_container.append(rename_title_button);
-
-
-  const task_container = document.createElement('div');
-  task_container.id = 'task-group-task-' + task_group_id + '-container';
-  task_container.classList.add('task-group-task-container');
-  const new_task_button = document.createElement('button');
-  new_task_button.innerText = 'new task';
-  new_task_button.classList.add('clickable');
-  new_task_button.onclick = _ => createNewTask({
-    parentContainer: task_container,
-    newTaskButton: new_task_button,
-  });
-  task_container.append(createTaskControlsContainer(new_task_button));
-
-
-  new_task_group.append(title_container, task_container);
+  //new_task_group.append(title_container, task_container);
+  const new_task_group = TaskGroupComponent(null);
   newTaskGroupButton.parentNode.replaceWith(new_task_group);
-  new_task_group.after(createDivider(), newTaskGroupButton.parentNode);
+  new_task_group.after(DividerComponent(), newTaskGroupButton.parentNode);
 
 
-  //Update local storage.
-  const project_id = projectContainer.id.split('-')[1];
-  let project = localStorage.getItem('project-' + project_id);
-  if (project === null) alert('bug detected, trying to create a task group for a project that doesn\'t exist in the localstorage.');
+  //Update the project data field in the local storage.
+  const value_string = localStorage.getItem(projectKey);
+  if (value_string === null) alert('bug detected, trying to create a task group for a project that does\'t exist in the local storage.');
   else {
-    //Parse the project into it's actual object.
-    project = JSON.parse(project);
 
-
-    //Update the tasks array for this project.
-    //@ts-ignore
-    project.data.push('task-group-' + task_group_id);
-
-
-    //Update the project in localstorage.
+    const stored_project = JSON.parse(value_string);
+    stored_project.data.push(new_task_group.id);
     localStorage.setItem(
-      'project-' + project_id,
-      JSON.stringify(project),
+      projectKey,
+      JSON.stringify(stored_project),
     );
-
   }
 
   //Create the `task group` item in localstorage.
   localStorage.setItem(
-    'task-group-' + task_group_id,
+    new_task_group.id,
     JSON.stringify({
-      name: rename_title_button.innerText,
+      name: new_task_group.id,
       tasks: [],
     }),
   );
@@ -454,6 +585,7 @@ function renameTitle({
 
 
   textfield.value = triggerButton.innerText;
+  textfield.classList.add('textfield');
   if (key.startsWith('project-') === true) textfield.classList.add('project-title');
   if (key.startsWith('task-') === true) textfield.classList.add('task-title');
   if (key.startsWith('task-group') === true) textfield.classList.add('task-group-title');
